9957f2541fdc893a015608717d5f3483b4cf1b8e
neel patel <neelpatel@neels-MacBook-Pro.local>
2022-05-28 18:45:22 -0400

    added

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,6 +4,22 @@ import { render } from 'react-dom';
 import css from './styles/style.styl'
 //import components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotGrid';
+
+//import react router dependencies 
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 
-render(<Main/>, document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path='/view/:postId' component={Single}></Route>
+        </Route>
+
+    </Router>
+)
+
+
+render(router, document.getElementById('root'));
