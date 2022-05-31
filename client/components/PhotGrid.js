64ab23c9bc1541ea0738dc5f8779f7104c4ee360
neel patel <neelpatel@neels-MacBook-Pro.local>
2022-05-30 22:28:53 -0400

    grid up

--- client/components/PhotGrid.js
+++ client/components/PhotGrid.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render(){
         return(
            <div className='photo-grid'>
-               <pre>
-                   {JSON.stringify(this.props.posts,null,' ')}
-               </pre>
+               {this.props.posts.map((posts,i) => <Photo {...this.props} key={i} i={i} posts={posts} />)}
            </div>
         )
     }
